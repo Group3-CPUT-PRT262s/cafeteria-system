bfc3ad0ee6e2c0a9d4c9dc8425dbf465286b9bfd
Tando Njikelana <thandonjikelana@protonmail.com>
2026-07-05 12:30:02 +0200

    backend changes: update to some of the controllers and a few visual additions in the login and registration changes.

--- src/main/java/com/group3/cafeteria_system/controller/CustomerController.java
+++ src/main/java/com/group3/cafeteria_system/controller/CustomerController.java
@@ -1,25 +1,16 @@
 package com.group3.cafeteria_system.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.group3.cafeteria_system.model.CustomerOrder;
-import com.group3.cafeteria_system.model.MenuItem;
-import com.group3.cafeteria_system.model.User;
+import com.group3.cafeteria_system.model.*;
 import com.group3.cafeteria_system.repository.TimeSlotRepository;
-import com.group3.cafeteria_system.service.MenuService;
-import com.group3.cafeteria_system.service.OrderService;
-import com.group3.cafeteria_system.service.UserService;
+import com.group3.cafeteria_system.service.*;
 
 @Controller
 public class CustomerController {
